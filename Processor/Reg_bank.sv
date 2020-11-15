57ecb3103ff34e87162b8b9f5979ee76be96a40d
reiracm <reiracm@gmail.com>
2020-11-14 22:15:40 -0600

    Update Reg_bank.sv
    Registers bank with 128 bits registers from 1100 to 1111

--- Processor/Reg_bank.sv
+++ Processor/Reg_bank.sv
@@ -1,13 +1,16 @@
 module Reg_bank(input logic clk, rst,
 					input logic we3,
 					input logic [3:0] ra1, ra2, wa3,
+					input logic [127:0] wev,
 					input logic [31:0] wd3,
 					output logic [31:0] rd1, rd2,
 					output reg [31:0] r_vga);
 
 logic [31:0] rf [14:0];
-//reg [31:0] r_vga;
+logic [127:0] rv [3:0];
+
 integer i;
+
 
 always_ff @(posedge clk)
 begin
@@ -16,6 +19,8 @@ begin
         for (i=0; i<15; i=i+1) rf[i] <= 32'b0;
 		  r_vga <= 32'b0;
       end 
+	else if(we3 > 4'b1100)
+		rv[wa3] <= wev;
 	else if (we3 & wa3 != 15) 
 		rf[wa3] <= wd3;
 	else

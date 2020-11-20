1b0df3d3f142ed1240b35796d7f7f3a513646aef
brianm.bra <brianm.bra.mm8@gmail.com>
2020-11-19 22:50:35 -0600

    mov vector operation added

--- Processor/mov_vector.sv
+++ Processor/mov_vector.sv
@@ -1,7 +1,17 @@
-// MOV module
-module mov_vector #(V = 128, N = 32)
-(
-input logic [V-1:0] inm
-);
-
-endmodule 
+module mov_vector #(parameter V = 128, N = 32) 
+				(input logic [N-1:0] src, 
+				input logic [1:0] imm,  output logic [V-1:0] dst);
+					
+	
+	always @(imm)
+	begin
+		case(imm)
+			0: dst[31:0] = src;
+			1: dst[63:32] = src;
+			2: dst[95:64] = src;
+			3: dst[127:96] = src;
+			default: dst = 'b0;
+		endcase
+	end
+	
+endmodule

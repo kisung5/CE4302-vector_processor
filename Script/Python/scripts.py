22acc69fc5456027365eba2473803a8edc7cb211
edmobe <31488944+edmobe@users.noreply.github.com>
2020-11-20 04:54:29 -0600

    Add fully functional nearest neighbor asm script (Python)

--- Script/Python/scripts.py
+++ Script/Python/scripts.py
@@ -1,5 +1,30 @@
 import numpy as np
 from PIL import Image
+import resource
+import sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29, -1))
+sys.setrecursionlimit(10**6)
+
+switch = '16'
+
+switches = {
+    '1': 0,
+    '2': 100,
+    '3': 200,
+    '4': 300,
+    '5': 40000,
+    '6': 40100,
+    '7': 40200,
+    '8': 40300,
+    '9': 80000,
+    '10': 80100,
+    '11': 80200,
+    '12': 80300,
+    '13': 120000,
+    '14': 120100,
+    '15': 120200,
+    '16': 120300,
+}
 
 registers = {
     "zero": 0,
@@ -9,8 +34,8 @@ registers = {
     "r4": 0,                   # New width = new height
     "r5": 0,                   # Pixel count
     "r6": 0,                   # Pixel counter
-    "r7": 0,
-    "r8": 0,
+    "r7": 0,                   # Pixel index
+    "r8": 0,                   # Image segment width
     "r9": 0,                   # Finished flag (0 when finished)
     "t1": 0,                   # Used to store values in vectors
     "t2": 0,                   # Used to test conditions
@@ -20,36 +45,23 @@ registers = {
     "v4": np.array([0] * 4),   # This vector will always be zero
 }
 
-'''
+memory = np.array([0] * (200000), dtype="uint8")
+
 inputImage = Image.open("image.jpg").convert('L')
 inputImage.load()
 inputImage = np.asarray(inputImage, dtype="uint8")
 outputImage = Image.fromarray(inputImage, 'L')
 outputImage.save('input.jpg')
-print(inputImage)
-'''
+inputImage = inputImage.flatten()
 
-memory = np.array([0] * (64 + 16))
-memory[0] = 1
-memory[1] = 2
-memory[2] = 3
-memory[3] = 4
-memory[4] = 5
-memory[5] = 6
-memory[6] = 7
-memory[7] = 8
-memory[8] = 9
-memory[9] = 10
-memory[10] = 11
-memory[11] = 12
-memory[12] = 13
-memory[13] = 14
-memory[14] = 15
-memory[15] = 16
-
+memory[0:160000] = inputImage[0:160000]
 
 def addi(dest: str, reg1: str, imm: int):
     registers[dest] = registers[reg1] + imm
+
+
+def add(dest: str, reg1: str, reg2: str):
+    registers[dest] = registers[reg1] + registers[reg2]
 
 
 def mod(dest: str, reg1: str, reg2: str):
@@ -100,49 +112,75 @@ def movv(dest: str, reg1: str, imm: int):
 
 
 def nearestNeighborInit():
-    addi('r1', 'zero', 4)   # Factor = 4
-    addi('r2', 'zero', 2)   # Increment = 2
-    addi('r3', 'zero', 4)   # Width = height = 400
-    addi('r4', 'zero', 8)   # New width = new height = 800
-    addi('r5', 'zero', 16)  # Pixel count = 160000
-    addi('r6', 'zero', 0)   # Pixel counter = 0
-    addi('r9', 'zero', 1)   # Finished flag = 1
-    addi('t1', 'zero', 16)  # Start at pixel count in memory (after input image)
-    movv('v1', 't1', 1)     # First index for the output index vector
-    addi('t1', 'zero', 17)  # Start at pixel count in memory (after input image)
-    movv('v1', 't1', 2)     # Second index for the output index vector
-    addi('t1', 'zero', 24)  # Start at pixel count in memory (after input image)
-    movv('v1', 't1', 3)     # Third index for the output index vector
-    addi('t1', 'zero', 25)  # Start at pixel count in memory (after input image)
-    movv('v1', 't1', 4)     # Fourth index for the output index vector
-    addi('t1', 'zero', 2)   # Constant value 2 for v3
+    # Factor = 4
+    addi('r1', 'zero', 4)
+    # Increment = 2
+    addi('r2', 'zero', 2)
+    # Width offset = height offset = 300
+    addi('r3', 'zero', 300)
+    # New width = new height = 200
+    addi('r4', 'zero', 200)
+    # Pixel count = 10000
+    addi('r5', 'zero', 10000)
+    # Pixel counter = 0
+    addi('r6', 'zero', 0)
+    # Pixel index = 0
+    addi('r7', 'zero', switches[switch])
+    # Image segment width = 100
+    addi('r8', 'zero', 100)
+    # Finished flag = 1
+    addi('r9', 'zero', 1)
+    # Start at pixel count in memory (after input image)
+    addi('t1', 'zero', 160000)
+    # First index for the output index vector
+    movv('v1', 't1', 1)
+    # Start at pixel count in memory (after input image)
+    addi('t1', 'zero', 160001)
+    # Second index for the output index vector
+    movv('v1', 't1', 2)
+    # Start at pixel count in memory (after input image)
+    addi('t1', 'zero', 160200)
+    # Third index for the output index vector
+    movv('v1', 't1', 3)
+    # Start at pixel count in memory (after input image)
+    addi('t1', 'zero', 160201)
+    # Fourth index for the output index vector
+    movv('v1', 't1', 4)
+    # Constant value 2 for v3
+    addi('t1', 'zero', 2)
     rep('v3', 't1')
     nearestNeighborLoop()
 
 
 def nearestNeighborLoop():
     if(not beq('r5', 'r6', 'nearestNeighborEnd')):    # Here the if-else statement must be omitted
-        lb('t1', 'r6', 0)
+        lb('t1', 'r7', 0)
         rep('v2', 't1')
         svi('v2', 'v1')
         addv('v1', 'v1', 'v3')
         addi('r6', 'r6', 1)
-        mod('t2', 'r6', 'r3')
+        addi('r7', 'r7', 1)
+        mod('t2', 'r6', 'r8')
         if(not beq('t2', 'zero', 'updateOffset')):
             if(not beq('zero', 'zero', 'nearestNeighborLoop')):
-                return
+                pass
 
 
 def updateOffset():
     rep('v2', 'r4')
     addv('v1', 'v1', 'v2')
+    add('r7', 'r7', 'r3')
     if not(beq('zero', 'zero', 'nearestNeighborLoop')):
         pass
 
 
 def nearestNeighborEnd():
-    print(memory)
+    pass
 
 
 # MAIN
 nearestNeighborInit()
+outputImage = memory[160000:200000]
+outputImage = outputImage.reshape(200, 200)
+outputImage = Image.fromarray(outputImage, 'L')
+outputImage.save('output' + switch + '.jpg')

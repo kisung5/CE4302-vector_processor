d664696c4e981d1ce139e572b3618d0df1d91529
brianm.bra <brianm.bra.mm8@gmail.com>
2020-11-20 22:25:06 -0600

    Change type in counter

--- Processor/counter.sv
+++ Processor/counter.sv
@@ -1,17 +1,20 @@
-module counter (input wen_v, clk, output logic stall);
-
-		logic [1:0] count; 
+module counter (input logic wen_v, clk, rst, 
+					 output logic stall, output logic [1:0] count);
 
 		always @(clk)
 		begin
-			if (wen_v) begin
+			if (rst) begin
+				count = 2'b00;
+				stall <= 1'b0;
+			end
+			else if (wen_v) begin
 				count = 2'b00;
 				stall <= 1'b1;
 			end
 			else if (count == 3)
 				stall <= 1'b0;
 			else if (stall) begin
-				count <= count + 1;
+				count = count + 1;
 				stall <= 1'b1; 
 			end
 		end
--- Processor/counter_tb.sv
+++ Processor/counter_tb.sv
@@ -1,13 +1,15 @@
 module counter_tb ();
 
 
-	logic wen_v, clk, stall;
-	counter counter (.wen_v(wen_v), .clk(clk), .stall(stall));
+	logic wen_v, clk, rst, stall;
+	logic [1:0] count;
+	counter counter (.wen_v(wen_v), .clk(clk), .rst(rst), .stall(stall), .count(count));
 	
 	always #10 clk <= ~clk;
 	
 initial begin
 	clk = 0;
+	rst = 0;
 	stall = 0;
 	wen_v = 0;
 	#10 wen_v = 1;

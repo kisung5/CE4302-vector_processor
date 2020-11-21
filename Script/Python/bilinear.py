b9ca36625f6d5e25c8d55db27f59b9d6d8e227a0
edmobe <31488944+edmobe@users.noreply.github.com>
2020-11-21 04:31:39 -0600

    Update high level bilinear algorithm for a 4x4 image

--- Script/Python/bilinear.py
+++ Script/Python/bilinear.py
@@ -1,69 +1,115 @@
+from PIL.Image import new
 import numpy as np
 
-memory = np.array([0] * (4 + 16), dtype='uint8')
-memory[0:4] = np.array([10, 20, 30, 40])
+memory = np.array([0] * (16 + 100), dtype=int)
+for i in range(0, 16):
+    memory[i] = (i + 1) * 10
 
-# ================= SCRIPT =================
+
+def printImage():
+    print(memory[16:272].reshape(10, 10))
+
+
+# ================= SCRIPT (INITIALIZATION) =================
 # Initial vector values
 v1 = np.array([0] * 4)
 v2 = np.array([0] * 4)
 v3 = np.array([2, 1, 2, 1])
 v4 = np.array([1, 2, 1, 2])
 
-# Initial register
-offset = 4
+# Width
+widthOffset = 4
 
-# Generate bigger image matrix
-v1 = np.array(memory[0:4])
-v2 = [0 + offset, 3 + offset, 12 + offset, 15 + offset]
+# Offset (image size, pixel count)
+memoryOffset = 16
 
-# Save it to memory
-for i in range(0, 4):
-    memory[v2[i]] = v1[i]
+# New width offset
+newWidthOffset = 10
 
-# Bilinear interpolation (first side)
-v1[0:4] = np.array([memory[0]] * 4)
-v2[0:2] = np.array([memory[1]] * 2)
-v2[2:4] = np.array([memory[2]] * 2)
-v1 = v1 * v3
-v1 = v1 // 3
-v2 = v2 * v4
-v2 = v2 // 3
-v1 = v1 + v2
-v2 = [1 + offset, 2 + offset, 4 + offset, 8 + offset]
+# Image counter
+imageCounter = 0
 
-# Save it to memory
-for i in range(0, 4):
-    memory[v2[i]] = v1[i]
+# Pixel counter
+pixelCounter = 0
 
-# Bilinear interpolation (second side)
-v2[0:4] = np.array([memory[3]] * 4)
-v1[0:2] = np.array([memory[2]] * 2)
-v1[2:4] = np.array([memory[1]] * 2)
-v1 = v1 * v3
-v1 = v1 // 3
-v2 = v2 * v4
-v2 = v2 // 3
-v1 = v1 + v2
-v2 = [13 + offset, 14 + offset, 7 + offset, 11 + offset]
 
-# Save it to memory
-for i in range(0, 4):
-    memory[v2[i]] = v1[i]
+# ================= SCRIPT (LOOP) =================
 
-# Bilinear interpolation (intermediate pixels)
-v1[0:2] = np.array([memory[offset + 4]] * 2)
-v1[2:4] = np.array([memory[offset + 8]] * 2)
-v2[0:2] = np.array([memory[offset + 7]] * 2)
-v2[2:4] = np.array([memory[offset + 11]] * 2)
-v1 = v1 * v3
-v1 = v1 // 3
-v2 = v2 * v4
-v2 = v2 // 3
-v1 = v1 + v2
-v2 = [5 + offset, 6 + offset, 9 + offset, 10 + offset]
+for pixelGroup in range(0, 9):
+    # Copy current values to the bigger image matrix
+    v1[0:2] = np.array(memory[imageCounter:imageCounter + 2])
+    v1[2:4] = np.array(memory[imageCounter + widthOffset:
+                              imageCounter + 2 + widthOffset])
+    v2 = [0 + memoryOffset,
+          3 + memoryOffset,
+          3 * newWidthOffset + memoryOffset,
+          3 + 3 * newWidthOffset + memoryOffset]
 
-# Save it to memory
-for i in range(0, 4):
-    memory[v2[i]] = v1[i]
-print(memory)
+    # Save it to memory
+    for i in range(0, 4):
+        memory[v2[i]] = v1[i]
+
+    # Bilinear interpolation (first side)
+    v1[0:4] = np.array([memory[imageCounter]] * 4)
+    v2[0:2] = np.array([memory[imageCounter + 1]] * 2)
+    v2[2:4] = np.array([memory[imageCounter + widthOffset]] * 2)
+    v1 = v1 * v3
+    v1 = v1 / 3
+    v2 = v2 * v4
+    v2 = v2 / 3
+    v1 = v1 + v2
+    v2 = [1 + memoryOffset,
+          2 + memoryOffset,
+          memoryOffset + newWidthOffset,
+          memoryOffset + 2 * newWidthOffset]
+
+    # Save it to memory
+    for i in range(0, 4):
+        memory[v2[i]] = v1[i]
+
+    # Bilinear interpolation (second side)
+    v2[0:4] = np.array([memory[imageCounter + widthOffset + 1]] * 4)
+    v1[0:2] = np.array([memory[imageCounter + widthOffset]] * 2)
+    v1[2:4] = np.array([memory[imageCounter + 1]] * 2)
+    v1 = v1 * v3
+    v1 = v1 / 3
+    v2 = v2 * v4
+    v2 = v2 / 3
+    v1 = v1 + v2
+    v2 = [memoryOffset + 3 * newWidthOffset + 1,
+          memoryOffset + 3 * newWidthOffset + 2,
+          memoryOffset + 1 * newWidthOffset + 3,
+          memoryOffset + 2 * newWidthOffset + 3]
+
+    # Save it to memory
+    for i in range(0, 4):
+        memory[v2[i]] = v1[i]
+
+    # Bilinear interpolation (intermediate pixels)
+    v1[0:2] = np.array([memory[memoryOffset + newWidthOffset]] * 2)
+    v1[2:4] = np.array([memory[memoryOffset + 2 * newWidthOffset]] * 2)
+    v2[0:2] = np.array([memory[memoryOffset + newWidthOffset + 3]] * 2)
+    v2[2:4] = np.array([memory[memoryOffset + 2 * newWidthOffset + 3]] * 2)
+    v1 = v1 * v3
+    v1 = v1 / 3
+    v2 = v2 * v4
+    v2 = v2 / 3
+    v1 = v1 + v2
+    v2 = [memoryOffset + newWidthOffset + 1,
+          memoryOffset + newWidthOffset + 2,
+          memoryOffset + 2 * newWidthOffset + 1,
+          memoryOffset + 2 * newWidthOffset + 2]
+
+    # Save it to memory
+    for i in range(0, 4):
+        memory[v2[i]] = v1[i]
+
+    imageCounter += 1
+    memoryOffset += 3
+    pixelCounter += 1
+
+    if pixelCounter % 3 == 0:
+        memoryOffset += 2 * newWidthOffset + 1
+        imageCounter += 1
+
+printImage()

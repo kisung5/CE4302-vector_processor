e997572e04362ca6308f10dfffdc4434b11e51cc
brianm.bra <brianm.bra.mm8@gmail.com>
2020-11-20 21:07:02 -0600

    Changes in mov

--- Processor/mov_vector_tb.sv
+++ Processor/mov_vector_tb.sv
@@ -1,17 +1,18 @@
 module mov_vector_tb #(parameter V = 128, N = 32);
 
-	logic [127:0] dst;
+	logic [127:0] dst, vector_input;
 	logic [31:0] src;
 	
 	logic [1:0] imm;
 	
-	mov_vector #(128, 32) op_mov (src, imm, dst);
+	mov_vector #(128, 32) op_mov (.src(src), .vector_input(vector_input), .imm(imm), .dst(dst));
 	
 	initial begin
-		src = 458; imm = 2'b00; #10;
-		src = 458; imm = 2'b01; #10;
-		src = 458; imm = 2'b10; #10;
-		src = 458; imm = 2'b11; #10;
+		vector_input = 128'hFFFF; #10
+		src = 128'hAAAA; imm = 2'b00; #10;
+		src = 458; 		  imm = 2'b01; #10;
+		src = 458;       imm = 2'b10; #10;
+		src = 458;       imm = 2'b11; #10;
 	end
 
 
